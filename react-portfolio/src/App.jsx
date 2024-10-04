b6bdbfc8a3baacad61ce0400f2accd0e2cce5ac1
JoeyVedder <qilotog1@gmail.com>
2024-10-04 17:51:11 -0400

    added header, footer, and app function

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Contact from './pages/Contact.jsx';
+import Resume from './pages/Resume.jsx';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} /> {/*When the user navigates to "/resume" in the browser URL, this route will render the <Resume /> component. */}
+
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    returm (
+    <footer className="footer">
+        <p>You can connect with me on:</p>
+        <ul>
+            <li><a href="">Github</a></li>
+            <li><a href="">LinkedIn</a></li>
+            <li><a href="">Stack Overflow</a></li>
+        </ul>
+    </footer>
+    );
+}
+
+export default Footer;
