63dec2d9290fbf8a97a74e39cff8da59fd3eaad7
JoeyVedder <qilotog1@gmail.com>
2024-11-17 22:15:04 -0500

    Added a couple more projects to my portfolio and changed the title and title icon.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -29,6 +29,21 @@ function Portfolio() {
             repoUrl: 'https://github.com/JoeyVedder/README-autogeneration',
             description: 'This is a command-line application that allows the user or client to generate a professional style README.' 
         },
+        {
+            title: 'QGLegacy',
+            repoUrl: 'https://github.com/JoeyVedder/QGLegacy',
+            description: 'This is a future gaming website that me and my friend marshall are currently working on. The languages included are Javascript, CSS, HTML. The goals with this website are applying multiple APIs to keep track of donations, and a API to keep track of when users follow our social media. It will display the games we have worked on, or are currently working on, along with a "scoreboard" for when users follow our socials and/or donate they will be displayed on a scoreboard.'
+        },
+        {
+            title: 'Weather Dashboard',
+            repoUrl: 'https://github.com/JoeyVedder/weatherdashboard-',
+            description: 'This is a website application that allows users to look up a city and get the temperature of the city for the day of along with the next 5 day forecast.'
+        },
+        {
+            title: 'Front-end Quiz',
+            repoUrl: 'https://github.com/JoeyVedder/Frontend-Quiz',
+            description: 'This website application was mainly focused on helping my friend Marshall learn the basics of frontend programming, the more I worked on it the more i realized how helpful it could be for other beginners.'
+        }
     ];
 
     return (
